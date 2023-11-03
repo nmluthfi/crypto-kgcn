18d9bc3ddce57ea2eb627d5ea527901fc5536871
nmluthfi <nurmuhammadluthfi@gmail.com>
2023-11-03 15:58:39 +0700

    v3 fix

--- src/main.py
+++ src/main.py
@@ -26,11 +26,11 @@ parser.add_argument('--dataset', type=str, default='crypto', help='which dataset
 parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
 parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
 parser.add_argument('--neighbor_sample_size', type=int, default=8, help='the number of neighbors to be sampled')
-parser.add_argument('--dim', type=int, default=16, help='dimension of user and entity embeddings')
-parser.add_argument('--n_iter', type=int, default=1, help='number of iterations when computing entity representation')
-parser.add_argument('--batch_size', type=int, default=128, help='batch size')
-parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
-parser.add_argument('--lr', type=float, default=5e-4, help='learning rate')
+parser.add_argument('--dim', type=int, default=64, help='dimension of user and entity embeddings')
+parser.add_argument('--n_iter', type=int, default=3, help='number of iterations when computing entity representation')
+parser.add_argument('--batch_size', type=int, default=256, help='batch size')
+parser.add_argument('--l2_weight', type=float, default=2e-5, help='weight of l2 regularization')
+parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
 parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 
 '''
@@ -62,7 +62,7 @@ parser.add_argument('--ratio', type=float, default=1, help='size of training dat
 '''
 
 
-show_loss = True
+show_loss = False
 show_time = True
 show_topk = True
 
